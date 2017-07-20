c734c9d660ae83f0225a5ad967b7f3689fe62b5d
joungtao <joungtao@gmail.com>
2017-07-20 23:17:09 +0800

    almost ok

--- gate.go
+++ gate.go
@@ -5,6 +5,7 @@
 package microserver
 
 import (
+	"fmt"
 	"log"
 	"sync"
 
@@ -41,75 +42,68 @@ func GateService(server rpc.IServer, symbols map[string]uint64) (uint64, rpc.Ser
 	return SYMBOL_GATE, &GateInst
 }
 
-func (s *GateImpl) observe(i ruid.RUID, k ruid.RUID, t ruid.RUID) (err error) {
+func (s *GateImpl) register(i ruid.RUID, k ruid.RUID, m uint64, t ruid.RUID) (err error) {
 	var buffer [binary.MaxVarintLen64]byte
-	_, err = ServerInst.Procedure(i, k, SYMBOL_HUB, SYMBOL_OBSERVE,
+	_, err = ServerInst.Procedure(i, k, SYMBOL_HUB, m,
 		append(buffer[:binary.PutUvarint(buffer[:], uint64(t))], []byte(ServerInst.Address)...))
 	return
 }
 
-func (s *GateImpl) ignore(i ruid.RUID, k ruid.RUID, t ruid.RUID) (err error) {
-	var buffer [binary.MaxVarintLen64]byte
-	_, err = ServerInst.Procedure(i, k, SYMBOL_HUB, SYMBOL_IGNORE,
-		append(buffer[:binary.PutUvarint(buffer[:], uint64(t))], []byte(ServerInst.Address)...))
-	return
-}
-
-func (s *GateImpl) handler(conn Gate) {
+func (s *GateImpl) handler(gate Gate) {
 	session := ruid.New()
 	comChan := make(chan []byte)
 	var buffer [binary.MaxVarintLen64 + 1]byte
 	buffer[0] = 0
 	if err := ServerInst.Distribute(session, 0, SYMBOL_SESSION, rpc.SYMBOL_CREATE,
 		buffer[:1+binary.PutUvarint(buffer[1:], SYMBOL_SESSION)], nil); err != nil {
-		log.Println("[Gate@%v] Create session error %v %v:\n>>>>%v", ServerInst.Address, conn.Address(), session, err)
+		log.Printf("[Gate@%v] Create session error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), session, err)
 	} else if err := ServerInst.Distribute(session, 0, SYMBOL_SESSION, rpc.SYMBOL_SYNCHRON, nil, comChan); err != nil {
-		log.Println("[Gate@%v] Synchron session error %v %v:\n>>>>%v", ServerInst.Address, conn.Address(), session, err)
+		log.Printf("[Gate@%v] Synchron session error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), session, err)
 	}
 	var components [][]byte
 	for component := range comChan {
 		components = append(components, component)
 	}
-	if err := s.observe(session, 0, session); err != nil {
-		log.Println("[Gate@%v] Observe session error %v %v:\n>>>>%v", ServerInst.Address, conn.Address(), session, err)
-	} else if err := conn.Send(rpc.SerializeSession(session, ServerInst.symbols, components...)); err != nil {
-		log.Println("[Gate@%v] Send session error %v %v:\n>>>>%v", ServerInst.Address, conn.Address(), session, err)
+	if err := s.register(session, 0, SYMBOL_OBSERVE, session); err != nil {
+		log.Printf("[Gate@%v] Observe session error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), session, err)
+	} else if err := gate.Send(rpc.SerializeSession(session, ServerInst.symbols, components...)); err != nil {
+		log.Printf("[Gate@%v] Send session error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), session, err)
 	}
 
 	GateInst.mutex.Lock()
-	s.gates[session] = conn
+	s.gates[session] = gate
 	GateInst.mutex.Unlock()
 
 	for {
-		if p, err := conn.Receive(); err != nil {
-			log.Println("[Gate@%v] Receive error %v %v:\n>>>>%v", ServerInst.Address, conn.Address(), session, err)
+		if p, err := gate.Receive(); err != nil {
+			log.Printf("[Gate@%v] Receive error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), session, err)
 			break
 		} else if i, o1 := binary.Uvarint(p); o1 <= 0 {
-			log.Println("[Gate@%v] Parse RUID error %v %v: %v %v", ServerInst.Address, conn.Address(), session, o1, p)
+			log.Printf("[Gate@%v] Parse RUID error %v %v: %v %v", ServerInst.Address, gate.Address(), session, o1, p)
 			break
 		} else if k, o2 := binary.Uvarint(p[o1:]); o2 <= 0 {
-			log.Println("[Gate@%v] Parse Key error %v %v: %v %v", ServerInst.Address, conn.Address(), session, o2, p[o1:])
+			log.Printf("[Gate@%v] Parse Key error %v %v: %v %v", ServerInst.Address, gate.Address(), session, o2, p[o1:])
 			break
 		} else if t, o3 := binary.Uvarint(p[o1+o2:]); o3 <= 0 {
-			log.Println("[Gate@%v] Parse Type error %v %v: %v %v", ServerInst.Address, conn.Address(), session, o3, p[o1+o2:])
+			log.Printf("[Gate@%v] Parse Type error %v %v: %v %v", ServerInst.Address, gate.Address(), session, o3, p[o1+o2:])
 			break
 		} else if m, o4 := binary.Uvarint(p[o1+o2+o3:]); o4 <= 0 {
-			log.Println("[Gate@%v] Parse method error %v %v: %v %v", ServerInst.Address, conn.Address(), session, o4, p[o1+o2+o3:])
+			log.Printf("[Gate@%v] Parse method error %v %v: %v %v", ServerInst.Address, gate.Address(), session, o4, p[o1+o2+o3:])
 			break
 		} else {
 			p = p[o1+o2+o3+o4:]
 			switch m {
 			case SYMBOL_OBSERVE:
-				if err := s.observe(i, k, session); err != nil {
-					log.Println("[Gate@%v] Observe %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[t], i, k, conn.Address(), session, err)
+				if err := s.register(i, k, SYMBOL_OBSERVE, session); err != nil {
+					log.Printf("[Gate@%v] Observe %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[t], i, k, gate.Address(), session, err)
 				}
 			case SYMBOL_IGNORE:
-				if err := s.ignore(i, k, session); err != nil {
-					log.Println("[Gate@%v] Ignore %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[t], i, k, conn.Address(), session, err)
+				if err := s.register(i, k, SYMBOL_IGNORE, session); err != nil {
+					log.Printf("[Gate@%v] Ignore %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[t], i, k, gate.Address(), session, err)
 				}
 			default:
 				if err := ServerInst.Distribute(i, k, t, m, p, nil); err != nil {
-					log.Println("[Gate@%v] Distribute %s(%v) to %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[m], m, ServerInst.symdict[t], i, k, conn.Address(), session, err)
+					log.Printf("[Gate@%v] Distribute %s(%v) to %s(%v:%v) error %v %v:\n>>>>%v", ServerInst.Address, ServerInst.symdict[m], m, ServerInst.symdict[t], i, k, gate.Address(), session, err)
 				}
 			}
 		}
@@ -118,13 +112,38 @@ func (s *GateImpl) handler(conn Gate) {
 	GateInst.mutex.Lock()
 	delete(s.gates, session)
 	GateInst.mutex.Unlock()
-	if err := s.ignore(session, 0, session); err != nil {
-		log.Println("[Gate@%v] Ignore session error %v:\n>>>>%v", ServerInst.Address, session, err)
+	if err := s.register(session, 0, SYMBOL_IGNORE, session); err != nil {
+		log.Printf("[Gate@%v] Ignore session error %v:\n>>>>%v", ServerInst.Address, session, err)
 	} else if err := ServerInst.Distribute(session, 0, SYMBOL_SESSION, rpc.SYMBOL_DESTROY, nil, nil); err != nil {
-		log.Println("[Gate@%v] Destroy session error %v:\n>>>>%v", ServerInst.Address, session, err)
+		log.Printf("[Gate@%v] Destroy session error %v:\n>>>>%v", ServerInst.Address, session, err)
 	}
 }
 
 func (s *GateImpl) Procedure(i ruid.RUID, method uint64, param []byte) (result []byte, err error) {
+	var j, k int
+	var m, n uint64
+	if n, j = binary.Uvarint(param); j <= 0 {
+		err = fmt.Errorf("[Gate@%v] Dispatch parse count error %v: %v", ServerInst.Address, j, param)
+		return
+	}
+	var observers []ruid.RUID
+	for l := uint64(0); l < n; l++ {
+		if m, k := binary.Uvarint(param[j:]); k <= 0 {
+			err = fmt.Errorf("[Gate@%v] Dispatch parse observer error %v: %v", ServerInst.Address, k, param[j:])
+			return
+		} else {
+			j += k
+			observers = append(observers, ruid.RUID(m))
+		}
+	}
+	param = param[j:]
+	for _, observer := range observers {
+		if gate, ok := s.gates[observer]; !ok {
+			log.Printf("[Gate@%v] Dispatch gate not found %v", ServerInst.Address, observer)
+		} else if err = gate.Send(param); err != nil {
+			log.Printf("[Gate@%v] Dispatch error %v %v:\n>>>>%v", ServerInst.Address, gate.Address(), err)
+			break
+		}
+	}
 	return
 }
--- hub.go
+++ hub.go
@@ -7,6 +7,7 @@ package microserver
 import (
 	"sync"
 
+	"encoding/binary"
 	"github.com/ibelie/rpc"
 	"github.com/ibelie/ruid"
 )
@@ -26,21 +27,115 @@ var (
 )
 
 type HubImpl struct {
-	mutex    sync.Mutex
-	services map[ruid.RUID]string
+	mutex     sync.Mutex
+	conns     map[string]*sync.Pool
+	observers map[ruid.RUID]map[string]map[ruid.RUID]bool
 }
 
-var HubInst = HubImpl{services: make(map[ruid.RUID]string)}
+var HubInst = HubImpl{conns: make(map[string]*sync.Pool), services: make(map[ruid.RUID]string)}
 
 func HubService(server rpc.IServer, symbols map[string]uint64) (uint64, rpc.Service) {
 	return SYMBOL_HUB, &HubInst
 }
 
+func (s *HubImpl) dispatch(i ruid.RUID, m uint64, p []byte) (err error) {
+	gates, ok := s.observers[i]
+	if !ok || len(gates) <= 0 {
+		return
+	}
+	for node, observers := range gates {
+		if len(observers) <= 0 {
+			continue
+		}
+		var buffer [(len(observers) + 2) * binary.MaxVarintLen64]byte
+		buflen := binary.PutUvarint(buffer[:], len(observers))
+		for observer, ok := range observers {
+			if ok {
+				buflen += binary.PutUvarint(buffer[buflen:], uint64(observer))
+			}
+		}
+		buflen += binary.PutUvarint(buffer[buflen:], m)
+		data := append(buffer[:buflen], p...)
+		if node == ServerInst.Address {
+			if gate, ok := s.local[SYMBOL_GATE]; !ok {
+				err = fmt.Errorf("[Hub@%v] Dispatch no local gate found: %v %v", ServerInst.Address, s.Node, s.local)
+			} else {
+				_, err = gate.Procedure(0, 0, data)
+			}
+			continue
+		}
+
+		if _, ok := s.conns[node]; !ok {
+			s.mutex.Lock()
+			s.conns[node] = &sync.Pool{New: func() interface{} {
+				if conn, err := net.DialTimeout("tcp", node, CONN_DEADLINE*time.Second); err != nil {
+					log.Printf("[Hub@%v] Connection failed: %s\n>>>>%v", ServerInst.Address, node, err)
+					return nil
+				} else {
+					return conn
+				}
+			}}
+			s.mutex.Unlock()
+		}
+
+		for j := 0; j < 3; j++ {
+			if o := s.conns[node].Get(); o == nil {
+				continue
+			} else if conn, ok := o.(net.Conn); !ok {
+				log.Printf("[Hub@%v] Connection pool type error: %v", ServerInst.Address, o)
+				continue
+			} else if err = conn.SetWriteDeadline(time.Now().Add(time.Second * WRITE_DEADLINE)); err != nil {
+			} else if _, err = conn.Write(data); err != nil {
+			} else {
+				s.conns[node].Put(conn)
+				break
+			}
+
+			if err != nil {
+				if err == io.EOF || isClosedConnError(err) {
+				} else if e, ok := err.(net.Error); ok && e.Timeout() {
+				} else {
+					log.Printf("[Hub@%v] Dispatch retry:\n>>>>%v", ServerInst.Address, err)
+				}
+			}
+		}
+	}
+
+	return
+}
+
 func (s *HubImpl) Procedure(i ruid.RUID, method uint64, param []byte) (result []byte, err error) {
 	switch method {
-	case SYMBOL_NOTIFY:
 	case SYMBOL_OBSERVE:
+		if g, j := binary.Uvarint(param); j <= 0 {
+			err = fmt.Errorf("[Hub@%v] Observe parse gate session error %v: %v", ServerInst.Address, j, param)
+		} else {
+			gate := string(param[j:])
+			s.mutex.Lock()
+			defer s.mutex.Unlock()
+			if _, ok := s.observers[i]; !ok {
+				s.observers[i] = make(map[string]map[ruid.RUID]bool)
+			}
+			if _, ok := s.observers[i][gate]; !ok {
+				s.observers[i][gate] = make(map[ruid.RUID]bool)
+			}
+			s.observers[i][gate][ruid.RUID(g)] = true
+		}
 	case SYMBOL_IGNORE:
+		if g, j := binary.Uvarint(param); j <= 0 {
+			err = fmt.Errorf("[Hub@%v] Ignore parse gate session error %v: %v", ServerInst.Address, j, param)
+		} else {
+			gate := string(param[j:])
+			s.mutex.Lock()
+			defer s.mutex.Unlock()
+			if gates, ok := s.observers[i]; ok {
+				if observers, ok := gates[gate]; ok {
+					delete(observers, ruid.RUID(g))
+				}
+			}
+		}
+	default:
+		s.dispatch(i, method, param)
 	}
 	return
 }
